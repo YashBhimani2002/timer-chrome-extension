b16f62d4a31ebddac357491fadba7802ce0e0580
YashQodic <129075913+YashBhimani2002@users.noreply.github.com>
2024-12-20 10:26:05 +0530

    Updated Popup UI

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -93,13 +93,18 @@ const Popup = () => {
     return (
         <div className="bg-gray-800 w-52 h-full p-2">
             <div className="flex flex-1 items-center mt-2 mb-3.5">
-                <p className=" text-white text-xl flex flex-1 w-full justify-center">{time.hours < 10 ? "0" + time.hours : time.hours}:{time.minutes < 10 ? "0" + time.minutes : time.minutes}:{time.seconds < 10 ? "0" + time.seconds : time.seconds}</p>
+                <span className=" text-white text-xl flex flex-1 w-full justify-center space-x-2">
+                    <p className="bg-gray-600 px-1 py-0.5">{time.hours < 10 ? "0" + time.hours : time.hours}</p>
+                    <p>:</p>
+                    <p className="bg-gray-600 px-1 py-0.5">{time.minutes < 10 ? "0" + time.minutes : time.minutes}</p>
+                    <p>:</p>
+                    <p className="bg-gray-600 px-1 py-0.5">{time.seconds < 10 ? "0" + time.seconds : time.seconds}</p></span>
                 <img src="../assets/resource/resetIcon.png" alt="reset" className="w-4 h-4 cursor-pointer rounded-lg" onClick={handleResetTimer} />
             </div>
             <div className="flex flex-col justify-center items-center gap-2">
                 <div className="flex gap-2 w-full">
                     <label className="text-white font-mono flex-1 self-start ">Read Time: </label>
-                    <input type="time" id="readTime" name="readTime" value={timerValues.readTime} min="00:00" max="59:59" step="1" className="flex-1 self-start " onChange={(e) => {
+                    <input type="time" id="readTime" name="readTime" value={timerValues.readTime} min="00:00" max="59:59" step="1" className="flex-1 self-start px-2 border-[1px] rounded-sm" onChange={(e) => {
                         console.log(e.target.value, "start time");
                         setTimerValues({ ...timerValues, readTime: e.target.value })
                     }} />
@@ -107,15 +112,15 @@ const Popup = () => {
                 <div className="flex gap-2 w-full">
                     <label className="text-white font-mono flex-1 self-start ">Break Time: </label>
                     <input type="time" id="breakTime" name="breakTime" value={timerValues.breakTime}
-                        min="0:00" max="59:59" step="1" className="flex-1 self-start " onChange={(e) => {
+                        min="0:00" max="59:59" step="1" className="flex-1 self-start px-2 border-[1px] rounded-sm" onChange={(e) => {
                             setTimerValues({ ...timerValues, breakTime: e.target.value })
                         }} />
                 </div>
             </div>
             <div className="flex gap-2 my-2">
-                <button className={`${time.hours == 0 && time.minutes == 0 && time.seconds == 0 ? "bg-green-500 hover:bg-green-300" : "bg-yellow-300 hover:bg-yellow-200"}  rounded-md border-transparent border-[1px] shadow-md h-8 w-full `} onClick={() => handleStartTimer(time.hours == 0 && time.minutes == 0 && time.seconds == 0 ? "start" : "resume")}>{time.hours == 0 && time.minutes == 0 && time.seconds == 0 ? "Start" : "Resume"}</button>
+                <button className={`${time.hours == 0 && time.minutes == 0 && time.seconds == 0 ? "bg-green-500 hover:bg-green-300" : "bg-yellow-300 hover:bg-yellow-200"}  rounded-[4px] border-transparent border-[1px] shadow-md h-8 w-full font-bold`} onClick={() => handleStartTimer(time.hours == 0 && time.minutes == 0 && time.seconds == 0 ? "start" : "resume")}>{time.hours == 0 && time.minutes == 0 && time.seconds == 0 ? "Start" : "Resume"}</button>
 
-                <button className=" bg-red-500 hover:bg-red-300 rounded-md border-transparent border-[1px] shadow-md h-8 w-full" onClick={() => handleStartTimer("stop")}>Stop</button>
+                <button className=" bg-red-500 hover:bg-red-300 rounded-[4px] border-transparent border-[1px] shadow-md h-8 w-full font-bold" onClick={() => handleStartTimer("stop")}>Stop</button>
             </div>
 
         </div>
